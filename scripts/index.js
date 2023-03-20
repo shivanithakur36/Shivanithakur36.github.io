13d0c6b1d6f2be1dd3cb44ea652b3de27bec391f
shivanithakur36 <shivanivijaythakur@gmail.com>
2023-03-20 21:13:06 +0530

    made responsive

---
+++ scripts/index.js
@@ -0,0 +1,25 @@
+
+
+let burger_icon = document.querySelector(".hamburger");
+
+burger_icon.addEventListener("click", 
+() =>{
+
+    let navbar =document.querySelector(".burger");
+
+    navbar.style.display="block";
+})
+
+
+
+let cross 
+= document.querySelector(".cross");
+
+cross.addEventListener("click",
+ ()=>{
+    
+    let navbar =document.querySelector(".burger");
+
+    navbar.style.display="none";
+}
+)
