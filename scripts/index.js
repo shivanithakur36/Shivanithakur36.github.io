5e940838c81f3d2e6253d0ca63c223099677fd55
shivanithakur36 <shivanivijaythakur@gmail.com>
2023-03-22 21:13:14 +0530

    added ham clicking

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,33 @@
 
 
-let burger_icon = document.querySelector(".hamburger");
+// let burger_icon = document.querySelector(".hamburger");
 
-burger_icon.addEventListener("click", 
-() =>{
+// burger_icon.addEventListener("click", 
+// () => {
 
-    let navbar =document.querySelector(".burger");
 
+let burger_icon = document.querySelector(".hi");
+burger_icon.addEventListener("click", () =>{
+    let navbar =document.querySelector(".bi");
     navbar.style.display="block";
 })
 
-
-
-let cross 
-= document.querySelector(".cross");
-
-cross.addEventListener("click",
- ()=>{
-    
-    let navbar =document.querySelector(".burger");
-
+let cross = document.querySelector(".crsi");
+cross.addEventListener("click", ()=>{
+    let navbar =document.querySelector(".bi");
     navbar.style.display="none";
-}
-)
+    console.log("www")
+})
+
+
+// let cross 
+// = document.querySelector(".cross");
+
+// cross.addEventListener("click",
+//  ()=>{
+    
+//     let navbar =document.querySelector(".burger");
+
+//     navbar.style.display="none";
+// }
+// )
